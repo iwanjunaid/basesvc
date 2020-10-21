b7dd06227c18f1f70c8a1cef595a5be60a37c2b2
Wibi Aristama <wibi.aristama@gmail.com>
2020-10-21 17:50:11 +0700

    add middleware recover

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/iwanjunaid/basesvc/config"
+	"github.com/mataharimall/notifications/src/http/middleware"
 
 	"github.com/iwanjunaid/basesvc/registry"
 
@@ -43,7 +44,7 @@ func (r *Rest) InitRouter() *fiber.App {
 
 	app.Use(cors.New())
 	app.Use(logger.New())
-	// app.Use(middleware.Recover()) // ?
+	app.Use(middleware.Recover())
 
 	registry := registry.NewRegistry(r.db)
 
