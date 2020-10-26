4e2c1db5f6d4d81a130bb6165859f1350f38af73
Kurniawan Junaidy <iwan.junaid@gmail.com>
2020-10-26 13:55:20 +0700

    Add migration scripts for table authors and articles.

--- cmd/migratepq.go
+++ cmd/migratepq.go
@@ -11,14 +11,14 @@ var migratePqCmd = &cobra.Command{
 	Use: "migratepq",
 	Run: func(cmd *cobra.Command, args []string) {
 		mig, err := migrate.New(
-			"./db/migrations/postgres",
+			"file://db/migrations/postgres",
 			"postgres://postgres:123456@127.0.0.1:5432/postgres?sslmode=disable&search_path=public")
 
 		if err != nil {
 			logger.Fatal(err)
 		}
 
-		if len(args) > 1 {
+		if len(args) >= 1 {
 			switch args[0] {
 			case "up":
 				if err := mig.Up(); err != nil {
--- db/migrations/postgres/000001_create_authors_table.up.sql
+++ db/migrations/postgres/000001_create_authors_table.up.sql
@@ -1,1 +1,8 @@
-DROP TABLE IF EXISTS authors;
+CREATE TABLE IF NOT EXISTS authors(
+  id          UUID PRIMARY KEY,
+  name        VARCHAR(255) NOT NULL,
+  email       VARCHAR(255) UNIQUE NOT NULL,
+  created_at  TIMESTAMP NOT NULL,
+  updated_at  TIMESTAMP DEFAULT NULL,
+  deleted_at  TIMESTAMP DEFAULT NULL
+);
