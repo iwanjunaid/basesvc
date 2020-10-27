92a04956fcdb67b3e4a81fbe5ad5cb48a32f1a27
Kurniawan Junaidy <iwan.junaid@gmail.com>
2020-10-27 09:13:43 +0700

    Revamp rest address construction.

--- cmd/rest.go
+++ cmd/rest.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/iwanjunaid/basesvc/config"
 	"github.com/iwanjunaid/basesvc/infrastructure/rest"
 	"github.com/spf13/cobra"
@@ -12,8 +14,12 @@ var restCommand = &cobra.Command{
 		defer logger.WithField("component", "api_command").Println("PreRun done")
 	},
 	Run: func(cmd *cobra.Command, args []string) {
+		var host = config.GetString("REST_HOST")
+		var port = config.GetInt("REST_PORT")
+		var address = fmt.Sprintf("%s:%d", host, port)
+
 		defer logger.WithField("component", "api_command").Println("Run done")
-		rest.NewRest(config.GetString("REST_PORT"), db).Serve()
+		rest.NewRest(address, db).Serve()
 	},
 	PostRun: func(cmd *cobra.Command, args []string) {
 		defer db.Close()
