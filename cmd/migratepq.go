4d1beb2ad3e734550f901da1398b0275c9f1fa00
Kurniawan Junaidy <iwan.junaid@gmail.com>
2020-10-27 09:09:17 +0700

    Migration for postgres.

--- cmd/migratepq.go
+++ cmd/migratepq.go
@@ -1,18 +1,34 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/iwanjunaid/basesvc/config"
 	"github.com/spf13/cobra"
 )
 
 var migratePqCmd = &cobra.Command{
 	Use: "migratepq",
 	Run: func(cmd *cobra.Command, args []string) {
+		var (
+			sourceURL   = config.GetString("POSTGRES_MIGRATION_DIR")
+			user        = config.GetString("POSTGRES_USER")
+			pass        = config.GetString("POSTGRES_PASS")
+			host        = config.GetString("POSTGRES_HOST")
+			port        = config.GetInt("POSTGRES_PORT")
+			db          = config.GetString("POSTGRES_DB")
+			sslMode     = config.GetString("POSTGRES_SSL_MODE")
+			searchPath  = config.GetString("POSTGRES_MIGRATION_SEARCH_PATH")
+			databaseURL = fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s&search_path=%s", user, pass,
+				host, port, db, sslMode, searchPath)
+		)
+
 		mig, err := migrate.New(
-			"file://db/migrations/postgres",
-			"postgres://postgres:123456@127.0.0.1:5432/postgres?sslmode=disable&search_path=public")
+			sourceURL,
+			databaseURL)
 
 		if err != nil {
 			logger.Fatal(err)
