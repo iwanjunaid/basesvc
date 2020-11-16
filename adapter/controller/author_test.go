bfc95f03a5e730f05b3df3c4d828f7f6d0ac047a
Martha Sutopo <martha.sutopo@gmail.com>
2020-11-16 10:40:08 +0700

    update code on adapter testing

--- adapter/controller/author_test.go
+++ adapter/controller/author_test.go
@@ -6,7 +6,11 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/iwanjunaid/basesvc/shared/mock/repository"
+
+	// adapter "github.com/iwanjunaid/basesvc/shared/mock/adapter"
+
+	repository "github.com/iwanjunaid/basesvc/shared/mock/repository"
+	"github.com/iwanjunaid/basesvc/usecase/author/interactor"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -15,12 +19,15 @@ func TestInsertAuthorController(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
 		repoAuthor := repository.NewMockAuthorSQLRepository(ctrl)
+		// adapter := adapter.NewMockAuthorController(ctrl)
 
 		Convey("Negative Scenarios", func() {
 			Convey("Should return error", func() {
 				repoAuthor.EXPECT().Create(context.Background(), nil).Return(nil, errors.New("error"))
-				err := NewAuthorController(AuthorController.InsertAuthor(nil))
-				So(err, ShouldNotBeNil)
+				auCtrl := interactor.NewAuthorInteractor(nil, interactor.AuthorSQLRepository(repoAuthor))
+				svc := NewAuthorController(auCtrl)
+				res := svc.InsertAuthor(nil)
+				So(res, ShouldNotBeNil)
 			})
 		})
 		// Convey("Positive Scenarios", func() {
@@ -32,8 +39,10 @@ func TestInsertAuthorController(t *testing.T) {
 		// 			UpdatedAt: time.Now(),
 		// 		}
 		// 		repoAuthor.EXPECT().Create(context.Background(), entAuthor).Return(entAuthor, errors.New("error"))
-		// 		err := NewAuthorController(AuthorController.InsertAuthor())
-		// 		So(err, ShouldNotBeNil)
+		// 		auCtrl := interactor.NewAuthorInteractor(nil, interactor.AuthorSQLRepository(repoAuthor))
+		// 		svc := NewAuthorController(auCtrl)
+		// 		res := svc.InsertAuthor(nil)
+		// 		So(res, ShouldBeNil)
 		// 	})
 		// })
 	})
